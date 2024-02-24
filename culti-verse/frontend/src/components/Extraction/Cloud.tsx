70dc38bb0c27b67136ffe37440034af0897782a4
jiangmizzz <2094197975@qq.com>
2024-02-25 02:18:20 +0800

    feat&chore: add word-cloud and echarts dependencies

---
+++ culti-verse/frontend/src/components/Extraction/Cloud.tsx
@@ -0,0 +1,114 @@
+import * as echarts from "echarts/core";
+import { TooltipComponent } from "echarts/components";
+import "echarts-wordcloud";
+import { useEffect } from "react";
+import { Box } from "@chakra-ui/react";
+
+//字体配色
+const fontColor = [
+  "dbbe9a",
+  "94617b",
+  "a0c1d2",
+  "96a48b",
+  "939391",
+  "eba79c",
+  "b0a7d7",
+  "d7c3b8",
+];
+
+interface cloudData {
+  value: number;
+  name: string;
+  nid: string;
+}
+
+export default function Cloud(props: {
+  data: cloudData[];
+  select: (nid: string) => void;
+}) {
+  echarts.use([TooltipComponent]);
+  useEffect(() => {
+    const chartDom = document.getElementById("wordcloud");
+    const myChart = echarts.init(chartDom);
+    const option = {
+      tooltip: {
+        show: true,
+        borderWidth: 0,
+        padding: [5, 10, 5, 10],
+        confine: true,
+        backgroundColor: "rgba(255,255,255, 0.6)",
+        textStyle: {
+          // color: "black",
+          lineHeight: 22,
+        },
+        extraCssText:
+          "box-shadow: 0 4px 20px -4px rgba(199, 206, 215, .7);border-radius: 4px;",
+      },
+      series: [
+        {
+          type: "wordCloud",
+          shape: "circle", //形状
+          keepAspect: true, //图像比例
+          //图像在画布中的位置
+          left: "center",
+          top: "center",
+          width: "90%",
+          height: "90%",
+          right: null,
+          bottom: null,
+
+          // Text size range
+          sizeRange: [8, 25],
+
+          //文字旋转角, range [-90, 90] by rotationStep 45
+          rotationRange: [0, 0],
+          rotationStep: 0,
+          //词间隔大小
+          gridSize: 5,
+
+          drawOutOfBound: false,
+          // If perform layout animation.
+          // NOTE disable it will lead to UI blocking when there is lots of words.
+          layoutAnimation: true,
+
+          // Global text style
+          textStyle: {
+            fontFamily: "sans-serif",
+            fontWeight: "bold",
+            // Color can be a callback function or a color string
+            // 函数式赋值令随机生效
+            color: function () {
+              return (
+                "#" + fontColor[Math.floor(Math.random() * fontColor.length)]
+              );
+            },
+          },
+          emphasis: {
+            focus: "self",
+          },
+          // Data is an array. Each array item must have name and value property.
+          data: props.data,
+        },
+      ],
+    };
+
+    if (chartDom && myChart) {
+      myChart.setOption(option);
+      //点击标签即可选中物像
+      myChart.on("click", function (params) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        props.select(params.data!.nid);
+      });
+    }
+
+    return () => {
+      if (myChart) {
+        myChart.dispose();
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <Box id="wordcloud" w={250} h={220} />;
+}
--- culti-verse/frontend/src/components/Extraction/Extraction.tsx
+++ culti-verse/frontend/src/components/Extraction/Extraction.tsx
@@ -1,7 +1,34 @@
 import { Box, Flex, HStack, Heading, StackDivider } from "@chakra-ui/react";
 import "./Extraction.css";
+import { useState } from "react";
+import Cloud from "./Cloud";
+const data = [
+  { value: 50, name: "华为" },
+  { value: 30, name: "VIVO" },
+  { value: 29, name: "OPPO" },
+  { value: 28, name: "HONOR" },
+  { value: 27, name: "红米" },
+  { value: 26, name: "小米" },
+  { value: 25, name: "美图" },
+  { value: 24, name: "ONEPLUS" },
+  { value: 23, name: "魅族" },
+  { value: 22, name: "红手指" },
+  { value: 21, name: "SAMSUNG" },
+  { value: 20, name: "金立" },
+  { value: 16, name: "BLACKBERRY" },
+  { value: 15, name: "诺基亚" },
+  { value: 14, name: "锤子" },
+  { value: 13, name: "大疆" },
+  { value: 12, name: "361" },
+  { value: 11, name: "摩托罗拉" },
+  { value: 10, name: "联想" },
+  { value: 9, name: "玩家国度" },
+];
 
 export default function Extraction() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [obj, setObj] = useState<string>(""); // 物像集中选中的物像
+  console.log(obj);
   return (
     <HStack
       className="extraction-main app-item-main"
@@ -9,10 +36,23 @@ export default function Extraction() {
       divider={<StackDivider borderColor="gray.200" />}
     >
       <Flex direction={"column"}>
-        <Heading as="h5" size="sm">
+        <Heading as="h5" size="sm" mb={1}>
           Element Feature
         </Heading>
-        <Box w={"250px"}></Box>
+        {/* 物像集 */}
+        <Box border="1px" borderColor="gray.200">
+          {obj === "" ? (
+            <Cloud
+              data={data.map((i) => {
+                return { name: i.name, value: i.value, nid: i.name };
+              })}
+              select={(selected) => setObj(selected)}
+            />
+          ) : (
+            <></>
+          )}
+        </Box>
+        {/* 画幅canvas */}
         <Box flexGrow={1}></Box>
       </Flex>
       {/* min-content yyds */}
