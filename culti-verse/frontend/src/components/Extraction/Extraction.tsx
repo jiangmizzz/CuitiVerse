9db4784569aab8aa17b9b14da33605b303af527e
jiangmizzz <2094197975@qq.com>
2024-02-26 23:31:14 +0800

    feat: add new component Noumenon

--- culti-verse/frontend/src/components/Extraction/Extraction.tsx
+++ culti-verse/frontend/src/components/Extraction/Extraction.tsx
@@ -1,19 +1,26 @@
 import {
+  AspectRatio,
   Box,
   // Button,
   Flex,
+  Grid,
+  GridItem,
   HStack,
   Heading,
   IconButton,
   StackDivider,
   Tooltip,
+  VStack,
 } from "@chakra-ui/react";
-import "./Extraction.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cloud from "./Cloud";
 import NetWork from "./Network";
-import { Edge, Node } from "../../vite-env";
+import { Edge, Node, normType } from "../../vite-env";
 import { ArrowBackIcon } from "@chakra-ui/icons";
+import Painting from "./Painting";
+import Noumenon from "./Noumenon";
+import { useExploreStore } from "../../stores/explore";
+
 const cloudData = [
   { value: 50, name: "华为" },
   { value: 30, name: "VIVO" },
@@ -65,27 +72,68 @@ const networkData: { nodes: Node[]; edges: Edge[] } = {
     return { ...edge, id: edge.from + "+" + edge.to };
   }),
 };
+const noumenonData = [
+  {
+    nid: "1",
+    name: "Monkey",
+    type: "single" as "single" | "combination",
+    metaphors: [
+      { type: "Identity", count: 1 },
+      { type: "Homophony", count: 1 },
+      { type: "Homophonic pun", count: 3 },
+      { type: "Synonym", count: 2 },
+      { type: "Homograph", count: 1 },
+      { type: "Satire", count: 1 },
+    ] as { type: normType; count: number }[],
+    positions: [
+      [1, 2, 3, 4],
+      [1, 2, 3, 4],
+    ],
+  },
+  {
+    nid: "2",
+    name: "Monkey / Bee ",
+    type: "combination" as "single" | "combination",
+    metaphors: [
+      { type: "Identity", count: 1 },
+      { type: "Homophony", count: 2 },
+      { type: "Homophonic pun", count: 0 },
+      { type: "Synonym", count: 2 },
+      { type: "Homograph", count: 3 },
+      { type: "Satire", count: 0 },
+    ] as { type: normType; count: number }[],
+    positions: [],
+  },
+];
 export default function Extraction() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const exploreStore = useExploreStore();
   const [obj, setObj] = useState<string>(""); // 词云图中选中的物像
   const [nid, setNid] = useState<string>(""); //网络图中选中的物像/组合
-  console.log(nid);
+  const [isList, setList] = useState<boolean>(true); //画作列表态
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [pid, setPid] = useState<string>(""); //选中的一幅画作id
+
+  //每次重选物像都将返回列表态
+  useEffect(() => {
+    setList(true);
+  }, [nid]);
+  console.log(pid);
   return (
     <HStack
       className="extraction-main app-item-main"
       align={"start"}
       divider={<StackDivider borderColor="gray.200" />}
     >
-      <Flex direction={"column"}>
-        <HStack mb={1}>
-          <Heading as="h5" size="sm" mb={1}>
+      <Flex direction={"column"} h={"100%"} gap={1}>
+        <HStack>
+          <Heading as="h5" size="sm">
             {"Element Feature"}
           </Heading>
           {obj !== "" && (
             <Tooltip label={"Return"} placement="right">
               <IconButton
                 color={"gray.500"}
-                // rounded={"full"}
+                h={"100%"}
                 size={"xs"}
                 aria-label={"return"}
                 icon={<ArrowBackIcon />}
@@ -112,23 +160,62 @@ export default function Extraction() {
           )}
         </Box>
         {/* 画幅canvas */}
-        <Box flexGrow={1}></Box>
-      </Flex>
-      {/* min-content yyds */}
-      <Box w={"min-content"}>
-        <Heading as="h5" size="sm" whiteSpace={"nowrap"}>
-          Element Selection
-        </Heading>
-        <Box minW={0}>
-          {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupicloudDatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. */}
+        <Box flexGrow={1} bgColor={"gray.200"} overflow={"auto"}>
+          {isList ? (
+            <Grid templateColumns={"repeat(3, 1fr)"} gap={1} p={1}>
+              {[
+                //TODO:添加空状态
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                // 12, 13, 14, 15, 16, 17, 18,19, 20,
+              ].map((pic) => {
+                return (
+                  <GridItem key={pic} bgColor={"gray.300"}>
+                    <AspectRatio ratio={1}>
+                      <img
+                        src={
+                          "https://www.freeimg.cn/i/2024/02/26/65dc8760d517a.jpg"
+                        }
+                      />
+                    </AspectRatio>
+                  </GridItem>
+                );
+              })}
+            </Grid>
+          ) : (
+            <Painting />
+          )}
         </Box>
-      </Box>
+      </Flex>
+      {/* min-content yyds 用以限制此列宽度被heading部分撑开*/}
+      <Flex w={"min-content"} direction={"column"} h={"100%"}>
+        <Heading as="h5" size="sm" whiteSpace={"nowrap"} mb={1}>
+          {"Element Selection"}
+        </Heading>
+        {/* 画作中的物像列表 */}
+        <Flex
+          flexGrow={1}
+          overflow={"auto"}
+          justify={"center"}
+          align={"center"}
+        >
+          <VStack spacing={5} w={"95%"} h={"100%"}>
+            {noumenonData.map((n) => {
+              return (
+                <Noumenon
+                  key={n.nid}
+                  nid={n.nid}
+                  name={n.name}
+                  occurences={n.positions.length}
+                  type={n.type}
+                  isSeleted={exploreStore.noumenon.nid === n.nid}
+                  metaphors={n.metaphors.filter((t) => t.count > 0)}
+                  select={() => exploreStore.setNoumenon(n.nid, n.name)}
+                />
+              );
+            })}
+          </VStack>
+        </Flex>
+      </Flex>
     </HStack>
   );
 }
