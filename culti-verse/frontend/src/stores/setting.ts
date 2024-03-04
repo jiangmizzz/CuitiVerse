60672284421b7992ebfd48e3eca0d409a47c6ca8
jiangmizzz <2094197975@qq.com>
2024-03-04 23:06:10 +0800

    feat: add new function: generateDesc() to store

--- culti-verse/frontend/src/stores/setting.ts
+++ culti-verse/frontend/src/stores/setting.ts
@@ -9,9 +9,11 @@ interface SettingState extends Settings {
   updateInfo: (newValue: Settings) => void;
   //重置至初始值
   reset: () => void;
+  //生成对个人文化背景的描述语段
+  generateDesc: () => string;
 }
 
-export const useSettingStore = create<SettingState>()((set) => ({
+export const useSettingStore = create<SettingState>()((set, get) => ({
   culture: "United States",
   age: 20,
   edu: "bachelor's degree",
@@ -38,5 +40,16 @@ export const useSettingStore = create<SettingState>()((set) => ({
         u2: 3,
       };
     });
+  },
+  generateDesc: () => {
+    return `The following is my personal cultural background: I come from ${
+      get().culture
+    }, I am ${get().age} years old this year, and my education level is a ${
+      get().edu
+    }. My level of understanding of Chinese culture is approximately ${
+      get().u1
+    } points (on a scale of 1 to 5), and my level of understanding of traditional Chinese painting is approximately ${
+      get().u2
+    } points (on a scale of 1 to 5). `;
   },
 }));
