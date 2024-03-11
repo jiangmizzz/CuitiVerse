e0bf988d258183f3ca48ca36b69feb860953be32
jiangmizzz <2094197975@qq.com>
2024-03-11 20:12:41 +0800

    feat: add word cloud series type

--- culti-verse/frontend/src/components/Extraction/Extraction.tsx
+++ culti-verse/frontend/src/components/Extraction/Extraction.tsx
@@ -1,7 +1,7 @@
 import {
   AspectRatio,
   Box,
-  // Button,
+  Text,
   Flex,
   Grid,
   GridItem,
@@ -18,19 +18,28 @@ import {
 import { useState } from "react";
 import Cloud from "./Cloud";
 import NetWork from "./Network";
-import { Edge, Node, PaintingType } from "../../vite-env";
+import {
+  CloudData,
+  Edge,
+  Node,
+  PaintingType,
+  seriesType,
+} from "../../vite-env";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import Painting from "./Painting";
 import Noumenon from "./Noumenon";
 import { useExploreStore } from "../../stores/explore";
 import useSWR from "swr";
 import { getFetcher, origin } from "../../utils/request";
+import { seriesIcon } from "../../stores/maps";
 
 //值得记录的写法+1
 type SimplePic = Pick<
   PaintingType,
   Exclude<keyof PaintingType, "noumenons" | "combinations">
 >;
+
+const series: seriesType[] = ["Animal", "Plant", "Fruit", "Other"];
 
 const picData1: PaintingType = {
   pid: "14",
@@ -51,11 +60,7 @@ const picData1: PaintingType = {
   ],
   combinations: [],
 };
-interface CloudDataRes {
-  times: number;
-  name: string;
-  nid: string;
-}
+
 export default function Extraction() {
   const exploreStore = useExploreStore();
   const [obj, setObj] = useState<string>(""); // 词云图中选中的物像id
@@ -65,9 +70,9 @@ export default function Extraction() {
   const [pid, setPid] = useState<string>(""); //选中的一幅画作id
 
   //获取词云图数据
-  const { data: cloudData, isLoading: cloudLoading } = useSWR<CloudDataRes[]>(
+  const { data: cloudData, isLoading: cloudLoading } = useSWR<CloudData[]>(
     "/objset/cloud",
-    getFetcher<CloudDataRes[]>
+    getFetcher<CloudData[]>
   );
   //获取网络图数据
   const { data: networkData, isLoading: networkLoading } = useSWR<{
@@ -94,7 +99,7 @@ export default function Extraction() {
       align={"start"}
       divider={<StackDivider borderColor="gray.200" />}
     >
-      <Flex direction={"column"} h={"100%"} gap={1}>
+      <Flex direction={"column"} align={"center"} h={"100%"} gap={1}>
         <HStack>
           <Heading as="h5" size="sm">
             {"Element Feature"}
@@ -120,12 +125,26 @@ export default function Extraction() {
                 <Spinner speed="0.8s" color="gray.300" />
               </Center>
             ) : (
-              <Cloud
-                data={(cloudData ? cloudData : []).map((i) => {
-                  return { name: i.name, value: i.times, nid: i.nid };
-                })}
-                select={(selected) => setObj(selected)}
-              />
+              <Flex direction={"column"} justify={"space-between"}>
+                <HStack flexGrow={1}>
+                  {series.map((s) => {
+                    return (
+                      <Flex align={"center"} gap={0.4}>
+                        <Image
+                          w={4}
+                          objectFit={"cover"}
+                          src={seriesIcon.get(s)}
+                        />
+                        <Text fontSize="xs">{s}</Text>
+                      </Flex>
+                    );
+                  })}
+                </HStack>
+                <Cloud
+                  data={cloudData ? cloudData : []}
+                  select={(selected) => setObj(selected)}
+                />
+              </Flex>
             )
           ) : networkLoading ? (
             <Center h={"100%"}>
@@ -143,7 +162,7 @@ export default function Extraction() {
           )}
         </Box>
         {/* 画幅canvas */}
-        <Box flexGrow={1} bgColor={"gray.100"} overflow={"auto"}>
+        <Box flexGrow={1} w={"100%"} bgColor={"gray.100"} overflow={"auto"}>
           {isList ? (
             picListLoading ? (
               <Center>
