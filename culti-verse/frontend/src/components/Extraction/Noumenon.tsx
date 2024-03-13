7b9e888015af19df4e1d4e3e7fa18035465711ee
jiangmizzz <2094197975@qq.com>
2024-03-14 00:56:25 +0800

    feat: improve noumenons list, display images carousel

--- culti-verse/frontend/src/components/Extraction/Noumenon.tsx
+++ culti-verse/frontend/src/components/Extraction/Noumenon.tsx
@@ -1,32 +1,87 @@
 import {
+  AspectRatio,
   Badge,
   Box,
   Flex,
   HStack,
   Heading,
-  Image,
+  Image as CImage,
   Tag,
   VStack,
 } from "@chakra-ui/react";
 import { NoumenonType } from "../../vite-env";
 import combination from "../../assets/group_element.svg";
 import { normColorMap } from "../../stores/maps";
+import { useEffect, useRef } from "react";
+import { origin } from "../../utils/request";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination } from "swiper";
+//添加指示点至swiper组件中
+SwiperCore.use([Pagination]);
 
 interface NoumenonProps extends NoumenonType {
-  occurences: number; //画中出现次数
+  occurences: number[][]; //画中出现的位置
   type: "single" | "combination";
+  paintingSrc: string; //画作src
   isSeleted: boolean; //当前是否被选中
   select: () => void;
 }
 
 export default function Noumenon(props: NoumenonProps) {
+  // const [refs, setRefs] = useState<RefObject<HTMLCanvasElement>[]>([]);
+  const refs = useRef<HTMLCanvasElement[]>([]);
+
+  //初始化
+  useEffect(() => {
+    //只有single才需要显示物像图
+    if (props.type === "single") {
+      const img = new Image();
+      img.src = origin + props.paintingSrc;
+      img.onload = function () {
+        const imageWidth_o = img.width; //原图像大小
+        const imageHeight_o = img.height;
+        props.occurences.map((pos, index) => {
+          const canvas = refs.current[index];
+          const context: CanvasRenderingContext2D = canvas.getContext("2d")!;
+          const cropImgWidth = imageWidth_o * pos[2]; //裁切后图像的大小
+          const cropImgHeight = imageHeight_o * pos[3];
+          let cropMax = 0;
+          //保持图像在画布中居中
+          if (cropImgWidth > cropImgHeight) {
+            cropMax = cropImgWidth;
+          } else {
+            cropMax = cropImgHeight;
+          }
+          canvas.width = cropMax;
+          canvas.height = cropMax;
+          canvas.style.width = "100%"; //显示尺寸
+          canvas.style.height = "100%";
+          //绘制图像
+          context.drawImage(
+            img,
+            imageWidth_o * pos[0],
+            imageHeight_o * pos[1],
+            cropImgWidth,
+            cropImgHeight,
+            (cropMax - cropImgWidth) / 2,
+            (cropMax - cropImgHeight) / 2,
+            cropImgWidth,
+            cropImgHeight
+          );
+        });
+      };
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <VStack w={"100%"} spacing={0.5} align={"end"}>
       <HStack align={"end"} justify={"space-between"} w={"90%"}>
         {/* 物像出现次数 */}
         {props.type === "single" ? (
           <Badge variant="solid" px={0.5} colorScheme="blackAlpha">
-            {props.occurences}
+            {props.occurences.length}
           </Badge>
         ) : (
           <Badge px={0.5} bg={"transparent"} />
@@ -35,20 +90,60 @@ export default function Noumenon(props: NoumenonProps) {
         <HStack align={"end"} spacing={1}>
           {props.metaphors.map((m) => {
             return (
-              <Box
+              <Badge
                 key={m.type}
-                w={3}
-                h={m.count * 3}
+                variant="solid"
+                rounded={"full"}
+                h={4}
+                w={4}
+                fontSize={"11px"}
+                textAlign={"center"}
                 bgColor={normColorMap.get(m.type)}
-                opacity={0.7}
-              />
+              >
+                {m.count}
+              </Badge>
             );
           })}
         </HStack>
       </HStack>
+      {props.type === "single" && (
+        <AspectRatio ratio={1} w={"90%"}>
+          <Box p={1} rounded={"md"} borderWidth={1} borderColor={"gray.200"}>
+            <Swiper
+              style={{
+                width: "100%",
+                height: "100%",
+                backgroundColor: "#EDF2F7",
+              }}
+              spaceBetween={30}
+              slidesPerView={1}
+              pagination={{
+                clickable: true,
+                dynamicBullets: true,
+              }}
+              autoplay={false}
+            >
+              {props.occurences.map((_, index) => {
+                return (
+                  <SwiperSlide key={_.toString()}>
+                    <canvas
+                      ref={(ele) => {
+                        if (ele) {
+                          //第一次为null, 需要条件判断
+                          refs.current[index] = ele;
+                        }
+                      }}
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </Box>
+        </AspectRatio>
+      )}
       <Flex w={"90%"} align={"center"}>
         {props.type === "combination" && (
-          <Image
+          <CImage
             src={combination}
             // 值得记录的写法*1
             w={5}
@@ -71,9 +166,14 @@ export default function Noumenon(props: NoumenonProps) {
           cursor={"pointer"}
           onClick={props.select}
         >
-          <Heading as="h6" size="xs" textAlign={"center"}>
-            {props.name}
-          </Heading>
+          <Flex direction={"column"}>
+            <Heading as="h6" size="xs" textAlign={"center"}>
+              {props.name[0]}
+            </Heading>
+            <Heading as="h6" size="xs" textAlign={"center"}>
+              {props.name[1]}
+            </Heading>
+          </Flex>
         </Tag>
       </Flex>
     </VStack>
