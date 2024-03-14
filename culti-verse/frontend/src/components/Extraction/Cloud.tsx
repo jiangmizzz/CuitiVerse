7023e3d75788e5b77afa8592b6997e2ded2edd89
jiangmizzz <2094197975@qq.com>
2024-03-15 00:01:42 +0800

    feat: divide word cloud to 4 series

--- culti-verse/frontend/src/components/Extraction/Cloud.tsx
+++ culti-verse/frontend/src/components/Extraction/Cloud.tsx
@@ -3,7 +3,8 @@ import { TooltipComponent } from "echarts/components";
 import "echarts-wordcloud";
 import { useEffect } from "react";
 import { Box } from "@chakra-ui/react";
-import { cloudData } from "../../vite-env";
+import icon from "../../assets/logo.png";
+import { CloudData } from "../../vite-env";
 
 //字体配色
 const fontColor = [
@@ -17,14 +18,49 @@ const fontColor = [
   "d7c3b8",
 ];
 
+const seriesCfg = {
+  type: "wordCloud",
+  shape: "cardioid", //形状
+  keepAspect: false, //图像比例
+
+  // Text size range
+  sizeRange: [5, 16],
+
+  //文字旋转角, range [-90, 90] by rotationStep 45
+  rotationRange: [0, 0],
+  rotationStep: 0,
+  //词间隔大小
+  gridSize: 5,
+  right: null,
+  bottom: null,
+
+  drawOutOfBound: false,
+  // If perform layout animation.
+  // NOTE disable it will lead to UI blocking when there is lots of words.
+  layoutAnimation: true,
+
+  // Global text style
+  textStyle: {
+    fontFamily: "sans-serif",
+    fontWeight: "bold",
+    // Color can be a callback function or a color string
+    // 函数式赋值令随机生效
+  },
+  emphasis: {
+    focus: "self",
+  },
+};
+
 export default function Cloud(props: {
-  data: cloudData[];
+  data: CloudData[];
   select: (nid: string) => void;
 }) {
   echarts.use([TooltipComponent]);
   useEffect(() => {
     const chartDom = document.getElementById("wordcloud");
     const myChart = echarts.init(chartDom);
+    const maskImage = new Image();
+    maskImage.src = icon;
     const option = {
       tooltip: {
         show: true,
@@ -41,61 +77,80 @@ export default function Cloud(props: {
       },
       series: [
         {
-          type: "wordCloud",
-          shape: "circle", //形状
-          keepAspect: true, //图像比例
+          ...seriesCfg,
           //图像在画布中的位置
-          left: "center",
-          top: "center",
-          width: "90%",
-          height: "90%",
-          right: null,
-          bottom: null,
-
-          // Text size range
-          sizeRange: [8, 25],
-
-          //文字旋转角, range [-90, 90] by rotationStep 45
-          rotationRange: [0, 0],
-          rotationStep: 0,
-          //词间隔大小
-          gridSize: 5,
-
-          drawOutOfBound: false,
-          // If perform layout animation.
-          // NOTE disable it will lead to UI blocking when there is lots of words.
-          layoutAnimation: true,
-
-          // Global text style
+          left: "0",
+          top: "0",
+          width: "50%",
+          height: "60%",
+          maskImage: maskImage, //遮罩图层
+          // Data is an array. Each array item must have name and value property.
           textStyle: {
-            fontFamily: "sans-serif",
-            fontWeight: "bold",
-            // Color can be a callback function or a color string
-            // 函数式赋值令随机生效
             color: function () {
-              return (
-                "#" + fontColor[Math.floor(Math.random() * fontColor.length)]
-              );
+              return "#" + fontColor[0];
             },
           },
-          emphasis: {
-            focus: "self",
+          data: props.data.filter((d) => d.type === "Animal")[0].data,
+        },
+        {
+          ...seriesCfg,
+          left: "20",
+          top: "130",
+          width: "60%",
+          height: "30%",
+          maskImage: maskImage,
+          // Data is an array. Each array item must have name and value property.
+          textStyle: {
+            color: function () {
+              return "#" + fontColor[3];
+            },
+          },
+          data: props.data.filter((d) => d.type === "Plant")[0].data,
+        },
+        {
+          ...seriesCfg,
+          left: "120",
+          top: "100",
+          width: "50%",
+          height: "30%",
+          maskImage: maskImage,
+          textStyle: {
+            color: function () {
+              return "#" + fontColor[2];
+            },
           },
           // Data is an array. Each array item must have name and value property.
-          data: props.data,
+          data: props.data.filter((d) => d.type === "Fruit")[0].data,
+        },
+        {
+          ...seriesCfg,
+          left: "120",
+          top: "0",
+          width: "70%",
+          height: "50%",
+          maskImage: maskImage,
+          textStyle: {
+            color: function () {
+              return "#" + fontColor[6];
+            },
+          },
+          // Data is an array. Each array item must have name and value property.
+          data: props.data.filter((d) => d.type === "Other")[0].data,
         },
       ],
     };
 
-    if (chartDom && myChart) {
-      myChart.setOption(option);
-      //点击标签即可选中物像
-      myChart.on("click", function (params) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //@ts-ignore
-        props.select(params.data!.nid);
-      });
-    }
+    maskImage.onload = function () {
+      if (chartDom && myChart) {
+        myChart.setOption(option);
+        //点击标签即可选中物像
+        myChart.on("click", function (params) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          //@ts-ignore
+          props.select(params.data!.nid);
+        });
+      }
+    };
 
     return () => {
       if (myChart) {
@@ -105,5 +160,5 @@ export default function Cloud(props: {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <Box id="wordcloud" w={250} h={220} />;
+  return <Box id={"wordcloud"} w={250} h={200} />;
 }
